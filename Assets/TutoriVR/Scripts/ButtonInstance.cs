afbd2d415aec37600b86b47196f6ec6c61df25f3
Lizzy Wang <yuchen.w@berkeley.edu>
2021-11-07 16:15:39 -0800

    added button instance and capture classes

---
+++ Assets/TutoriVR/Scripts/ButtonInstance.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonInstance : MonoBehaviour
+{
+    // private ?? script = script being run;
+    private Transform leftControllerPos;
+    private Transform rightControllerPos;
+    private bool rightTriggerDown;
+    private bool leftTriggerDown;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // gets data of current frame
+    void createInstance()
+    {
+        // script = check script that is running
+        leftControllerPos = TiltbrushAppInfo.GetLeftController();
+        rightControllerPos = TiltbrushAppInfo.GetRightController();
+        rightTriggerDown = TiltbrushAppInfo.GetRightTriggerDown();
+        leftTriggerDown = TiltbrushAppInfo.GetLeftTriggerDown();
+    }
+}
---
+++ Assets/TutoriVR/Scripts/TrackButtons.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackButtons : MonoBehaviour
+{
+    private Dictionary<int, ButtonInstance> captures; //dictionary: frame to button instance class
+    // Start is called before the first frame update
+    void Start()
+    {
+        captures = new Dictionary<int, ButtonInstance>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        captures[Time.frameCount] = ButtonInstance.createInstance();
+    }
+}
