a09d5937bee95376c10d1ad16195e9e2e9c337f8
Steven Mo <mostevenj@gmail.com>
2021-11-14 18:28:25 -0800

    recording

--- Assets/TutoriVR/Scripts/Record.cs
+++ Assets/TutoriVR/Scripts/Record.cs
@@ -1,15 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System.Threading;
+using System.Runtime.InteropServices;
+using RockVR.Video;
 
+[RequireComponent(typeof(Camera))]
 public class Record : MonoBehaviour, Runnable
 {
     [SerializeField] Material recordButton;
     [SerializeField] Material stopButton;
     private bool recording;
+
+    // public RenderTexture videoRenderTexture;
+    // public TextureFormat saveFormat = TextureFormat.RGB24;
+
+    // private Camera captureCamera;
+    // public float deltaFrameTime;
+    // public string filePath;
+
+    // public VideoCapture vidcapture;
+
+    // protected void Awake()
+    // {
+    //     captureCamera = GetComponent<Camera>();
+    //     deltaFrameTime = 1f / 30;
+    // }
+
     // Start is called before the first frame update
     void Start()
     {
+        // vidcapture = new VideoCapture();
         recording = false;
     }
 
@@ -18,7 +40,7 @@ public class Record : MonoBehaviour, Runnable
     {
         if (recording)
         {
-
+            // vidcapture.StartCapture();
         }
     }
 
@@ -27,12 +49,19 @@ public class Record : MonoBehaviour, Runnable
         if (recording)
         {
             recording = false;
+            VideoCaptureCtrl.instance.StopCapture();
             gameObject.GetComponent<Renderer>().material = recordButton;
             Debug.Log("Save & Export");
         }
         else
         {
             recording = true;
+
+            VideoCaptureCtrl.instance.StartCapture();
+            // VideoPlayer.instance.SetRootFolder();
+            // VideoPlayer.instance.NextVideo();
+            // VideoPlayer.instance.PlayVideo();
+
             gameObject.GetComponent<Renderer>().material = stopButton;
             Debug.Log("Start Recording");
         }
