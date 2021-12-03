f1f0dfca4d5bb0cc56bdb56ec7a7e3b8829f9745
NicholasJJ <nicholasjohnjennings@gmail.com>
2021-12-02 20:00:49 -0800

    Add Camera tracking and 2 cameras

---
+++ Assets/FollowHMD.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowHMD : MonoBehaviour
+{
+    public Transform HMD;
+    public float xOffset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = HMD.position;
+        transform.rotation = HMD.rotation;
+
+        transform.position += transform.right * xOffset;
+    }
+}
